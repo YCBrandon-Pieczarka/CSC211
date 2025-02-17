0596c1f655ab3942b817ea9ea60e05cce1769c99
YCBrandon-Pieczarka <bpieczar@yc.edu>
2025-02-16 21:46:43 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+// Name and Number:
+$first = "Brandon";
+$last = "Pieczarka";
+$phone = 9282779551;
+
+// Full Address:
+$street = "8500 East Lakeshore Drive";
+$city = "Prescott Valley";
+$state = "AZ";
+$zip = 86314;
+
+// Print Contact Card:
+print "<p>Contact Card:<br>$first $last<br>$street <br>$city $state $zip<br>Phone: $phone </p>";
+
+?>
+</body>
+</html>
